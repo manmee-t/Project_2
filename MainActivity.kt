a6e6d656574af4a22a36c2d77f89569d609f0693
Manmeet Ray <manmeetkumar7879@gmail.com>
2024-09-26 18:14:47 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.practice
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,31 +23,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val l1 = listOf(
+        val l1 = mutableListOf<String>(
             "Manmeet",
             "Priyanshu",
             "Sourav",
             "Arpit"
         )
         val textView = findViewById<TextView>(R.id.textView)
+        val listItem = findViewById<TextView>(R.id.textView2)
+        val add = findViewById<EditText>(R.id.editTextView1)
         val num = findViewById<EditText>(R.id.editTextNumber)
-        num.setOnEditorActionListener {
-            _, _, _ ->
+        val btn = findViewById<Button>(R.id.button)
+        listItem.setText(l1.toString())
+        num.setOnEditorActionListener { _, _, _ ->
             val input = num.text.toString()
             val index = input.toIntOrNull()
             if (index != null && index in l1.indices) {
                 textView.setText(l1[index])
-            }else{
+            } else {
                 textView.setText("Invalid Input")
             }
             true
         }
+        btn.setOnClickListener {
+            val input = add.text.toString()
+            l1.add(input)
+            add.setText("")
+            listItem.setText(l1.toString())
+            Toast.makeText(this, "You added $input", Toast.LENGTH_SHORT).show()
+        }
     }
-
-   // fun input(p0: EditText) {}
 }
-
-/*fun number(num:Int=0): Int{
-    val input = readln().toInt()
-    return input
-}*/
